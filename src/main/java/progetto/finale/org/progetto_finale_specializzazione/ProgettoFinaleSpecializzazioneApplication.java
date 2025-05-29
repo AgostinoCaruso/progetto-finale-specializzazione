998c0af0e2ef5c2faa0d20c3ebd33a022cdc880f
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-05-29 22:45:04 +0200

    first commit

---
+++ src/main/java/progetto/finale/org/progetto_finale_specializzazione/ProgettoFinaleSpecializzazioneApplication.java
@@ -0,0 +1,13 @@
+package progetto.finale.org.progetto_finale_specializzazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFinaleSpecializzazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFinaleSpecializzazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/progetto/finale/org/progetto_finale_specializzazione/ProgettoFinaleSpecializzazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package progetto.finale.org.progetto_finale_specializzazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFinaleSpecializzazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
